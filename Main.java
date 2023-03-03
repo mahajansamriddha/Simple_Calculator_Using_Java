86c123d03de77739c9a9e92a1720d9b976906f04
Samriddha Mahajan <125155821+mahajansamriddha@users.noreply.github.com>
2023-03-03 13:20:01 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main{
+    static void Addition(){
+       int a=10;
+       int b=2;
+       System.out.println("Addition:"+(a+b));
+    }
+    static void Subtraction(){
+        int a=10;
+        int b=2;
+        System.out.println("Subtraction:"+(a-b));
+    }
+    static void Division(){
+        int a=10;
+        int b=2;
+        System.out.println("Division:"+(a/b));
+    }
+    static void Multiplication(){
+        int a=10;
+        int b=2;
+        System.out.println("Multiplication:"+(a*b));
+    }
+    public static void main(String[] args){
+        Addition();
+        Subtraction();
+        Division();
+        Multiplication();
+    }
+}
